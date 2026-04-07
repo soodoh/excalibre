02b29d2d0266b8f9cff980e388de6e787b4817aa
Paul DiLoreto <soodohh@pm.me>
2026-04-07 14:39:40 -0700

    chore: stop tracking generated astro cache

--- apps/docs/.astro/content-modules.mjs
+++
@@ -1,5 +0,0 @@
-
-export default new Map([
-["src/content/docs/getting-started.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started.mdx&astroContentModuleFlag=true")],
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")]]);
-		
