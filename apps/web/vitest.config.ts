2bd72e69fb4f20fe9411decf4e0280f66efd2e3b
Paul DiLoreto <soodohh@pm.me>
2026-04-09 19:15:42 -0700

    fix: increase test timeout to handle node+browser concurrency

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	plugins: [tsconfigPaths()],
 	test: {
+		testTimeout: 15000,
 		coverage: {
 			provider: "v8",
 			reporter: ["text", "text-summary", "html"],
